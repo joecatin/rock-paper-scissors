1b4e9fe712446d8c746f3b0cddd0cfcd9b28b5b5
bautheac <o.bautheac@gmail.com>
2021-07-30 10:40:35 +0100

    Add interface

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
-// Randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
 
+let [rounds, player, computer, draw] = [0, 0, 0 , 0];
+let results = [];
+let possibleWinner = ["player", "computer", "draw"];
+
+
+// Randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
 computerPlay = () => {
 
     // Draw a random number between 1 and 3.
@@ -45,45 +50,45 @@ playRound = (playerSelection, computerSelection) => {
     case "rock": 
       switch(computer) {
         case "rock": 
-          console.log("It's a draw: rock vs rock.");
+          // console.log("It's a draw: rock vs rock.");
           outcome = "draw";
           break;
         case "paper": 
-        console.log("You lose! Paper beats rock.");
-          outcome = "lose";
+          // console.log("You lose! Paper beats rock.");
+          outcome = "computer";
           break;
         default:
-          console.log("You win! Rock beats scissors.");
-          outcome = "win";
+          // console.log("You win! Rock beats scissors.");
+          outcome = "player";
       }
       break;
     case "paper":
       switch(computer) {
         case "rock": 
-          console.log("You win! Paper beats rock.");
-          outcome = "win";
+          // console.log("You win! Paper beats rock.");
+          outcome = "player";
           break;
         case "paper": 
-          console.log("It's a draw: paper vs paper.");
+          // console.log("It's a draw: paper vs paper.");
           outcome = "draw";
           break;
         default:
-          console.log("You lose! Paper beats scissors.");
-          outcome = "lose";
+          // console.log("You lose! Paper beats scissors.");
+          outcome = "computer";
       }
       break;
     default:
       switch(computer) {
         case "rock": 
-          console.log("You lose! Rock beats Scissors.");
-          outcome = "lose";
+          // console.log("You lose! Rock beats Scissors.");
+          outcome = "computer";
           break;
         case "paper": 
-          console.log("You win! Scissors beat paper.");
-          outcome = "win";
+          // console.log("You win! Scissors beat paper.");
+          outcome = "player";
           break;
         default:
-          console.log("It's a draw: scissors vs scissors.");
+          // console.log("It's a draw: scissors vs scissors.");
           outcome = "draw";
       }
   }
@@ -94,9 +99,9 @@ playRound = (playerSelection, computerSelection) => {
 // Check if player input is rock, paper or scissors regardless
 // of case.
 // Declare array with possible hands as global variables
-let hands = ["rock", "paper", "scissors"];
+let possibleHands = ["rock", "paper", "scissors"];
 checkPlayerInput = (input) => {
-  return hands.includes(input);  
+  return possibleHands.includes(input);  
 }
 
 // Count number of occurrences of a value in an array
@@ -108,50 +113,200 @@ determineWinner = (results) => {
   // Count the number of win and lose in the results array
   // If number of win > number of lose player wins
   // else computer wins.
-  
-  let wins = countOccurrences(results, "win");
-  let loses = countOccurrences(results, "lose");
+
+  let player = countOccurrences(results, "player");
+  let computer = countOccurrences(results, "computer");
   let draws = countOccurrences(results, "draw");
 
   let outcome;
-  if (wins > loses) {
-    outcome = ["Player", wins, loses, draws];
-  } else if (wins < loses) {
-    outcome = ["Computer", loses, wins, draws];
+  if (player > computer) {
+    outcome = ["Player", player, computer, draws];
+  } else if (player < computer) {
+    outcome = ["Computer", computer, player, draws];
   } else {
-    outcome = ["draw", wins, loses, draws];
+    outcome = ["draw", player, computer, draws];
   }
 
   return outcome
 }
 
 
-// Plays a five round game with result logged out to the console for 
-// each round and winner logged out to the console at the end.
-game = () => {
-  
-  let results = [];
-  let playerHand;
+showHands = (player, computer) => {
+  document.querySelector(`div[id='player'] img`).
+    setAttribute("src", `assets/images/hand-${player}-player.png`);
 
-  for(i = 0; i < 5; i++) {
+  if (Math.random() > 0.5) {
+    document.querySelector(`div[id='vs'] img`).
+      setAttribute("src", "assets/images/lightening-player-computer.png");
+  } else {
+    document.querySelector(`div[id='vs'] img`).
+      setAttribute("src", "assets/images/lightening-computer-player.png");
+  };
 
-    let keepGoing = true;
-    while (keepGoing){
-      playerHand = prompt("Please pick a hand: 'rock', 'paper' or 'scissors'", "rock").toLowerCase();
-      keepGoing = !checkPlayerInput(playerHand);
-    }
-    
-    results.push(playRound(playerHand, computerPlay()));
- }
-
- let winner = determineWinner(results);
-
-
- let outcome = ((winner[0] == "draw")? ("It's a draw, both player and computer won " + winner[1] + " round" + ((winner[1] > 0)? "s" : "")) :
- (winner[0] + " wins with " + winner[1] + " round" + ((winner[1] > 1)? "s" : "") + " won vs. " + winner[2])) + 
- ((winner[3] > 0)? ("; " + winner[3] + " draw" + ((winner[3] > 1)? "s" : "") + ".") : ".");
- 
- console.log(outcome);
+  document.querySelector(`div[id='computer'] img`).
+    setAttribute("src", `assets/images/hand-${computer}-computer.png`);
 }
 
-game();
+updateScore = (winner) => {  
+  eval(winner + "++;");
+  document.querySelector(`div[id='${winner}'] span[id='value']`).
+    textContent = eval(winner);
+}
+
+showHandWinner = (winner) => {
+
+  if (winner == "draw"){
+    document.getElementById("round").
+      innerHTML = "<span id='left'>draw</span><span id='right'>...</span>";
+  } else {
+    document.getElementById("round").style.setProperty("color", `var(--${winner})`);
+    document.getElementById("round").
+      innerHTML = `<span>${winner} wins!</span>`;
+  }
+}
+
+
+showWinner = (winner, outcome) => {
+
+  let target = document.querySelector(".scores");
+  let colour, ruoloc;
+  
+  if (winner == "draw") {
+    colour = (Math.random() > 0.5)? "player" : "computer"
+  } else {
+    colour = winner;    
+  }
+
+  ruoloc = possibleWinner.filter(i => ![colour, "draw"].includes(i))[0];
+  target.style.setProperty("justify-content", "center");
+  target.innerHTML = `<span>${outcome}</span>`;
+  target = document.querySelector(".scores span");
+  target.style.setProperty("color", `var(--${colour})`);
+  target.style.setProperty("border", `.4rem solid var(--${ruoloc})`);
+  target.style.setProperty("border-radius", "1rem");  
+  target.style.setProperty("margin", "5rem");  
+  target.style.setProperty("padding", "1rem .5rem");  
+  target.style.setProperty("background-color", "rgba(255, 255, 255, 0.75)");  
+  target.classList.add("blink");
+}
+
+reset = () => {
+  [rounds, player, computer, draw] = [0, 0, 0, 0];
+  results = [];
+
+  let target = document.querySelector(".scores");
+  target.innerHTML = `
+    <div id="set">
+      <div id="set-box">
+          <div id="computer" class="score">
+              <span id="label">computer</span>
+              <span id="value">0</span>
+          </div>
+          <div id="player" class="score">
+              <span id="label">player</span>  
+              <span id="value">0</span>  
+          </div>    
+          <div id="draw" class="score">
+              <span id="label">draw</span>  
+              <span id="value">0</span>  
+          </div>
+      </div>                         
+    </div>
+
+    <div id="round"></div>
+  `;
+
+target = document.querySelector("div[id='player'] img");
+target.setAttribute("src", "assets/images/hand-start-player.png");
+
+target = document.querySelector("div[id='computer'] img");
+target.setAttribute("src", "assets/images/hand-start-computer.png");
+}
+
+removeEventListeners = () => {
+  let hands = document.querySelectorAll(".hand");
+  
+  hands.forEach(hand => {
+    hand.removeEventListener("click", (e) => {
+      playSound(e);
+      playGame(e);
+    })
+  }); 
+  
+  hands.forEach(hand => {
+    hand.removeEventListener("transitionend", function(e){ removeTransition(this) })
+  }); 
+}
+
+terminateGame = (results) => {
+
+  removeEventListeners();
+
+  let winner = determineWinner(results);
+
+  let outcome = (
+      (winner[0] == "draw")? 
+      ("It's a draw, both player and computer won " + winner[1] + " round" + 
+      ((winner[1] > 1)? "s" : "")) :
+      (winner[0] + " wins with " + winner[1] + " round" + 
+      ((winner[1] > 1)? "s" : "") + " won vs. " + winner[2])
+    ) + 
+    (
+      (winner[3] > 0)? ("; " + winner[3] + " draw" + 
+      ((winner[3] > 1)? "s" : "") + ".") : "."
+    );
+
+
+  showWinner(winner[0].toLowerCase(), outcome);
+  setTimeout(reset, 1750);
+}
+
+// Plays a five round game with result logged out to the console for 
+// each round and winner logged out to the console at the end.
+playGame = (e) => {
+
+  let playerHand = e.path[1].id;
+  let computerHand = computerPlay();
+  let result = playRound(playerHand, computerHand);
+
+  showHands(playerHand, computerHand);
+
+  updateScore(result);
+  results.push(result);
+
+  showHandWinner(result) 
+
+  rounds++;
+
+  if (rounds == 5) setTimeout( () => { terminateGame(results) }, 400)  
+}
+
+
+let playSound = (e) => {
+                
+  const audio = document.querySelector(`audio[id="${e.path[1].attributes["sound-key"].value}"]`);
+  const hand = document.querySelector(`div[id="${e.path[1].attributes["id"].value}"]`);
+  // console.log(audio);
+
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();   
+  
+  hand.classList.add("playing");
+}
+
+let removeTransition = (hand) => { hand.classList.remove(("playing")); }
+
+let hands = document.querySelectorAll(".hand");
+
+hands.forEach(hand => {
+  hand.addEventListener("click", (e) => {
+    playSound(e);
+    playGame(e);
+  })
+}); 
+
+hands.forEach(hand => {
+  hand.addEventListener("transitionend", function(e){ removeTransition(this) })
+}); 
